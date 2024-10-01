c54a03bd7a98d215abaf4e4658c73682627a5b82
Belgacem-Dahmen <belgacem.dahmen@gmail.comn>
2024-10-01 17:47:28 +0200

    docs : update cuboide function docs

--- volumeOfCuboide.js
+++ volumeOfCuboide.js
@@ -1,5 +1,15 @@
+/**
+ * return a volume of cube
+ * @param {number} l 
+ * @param {number} w 
+ * @param {number} h 
+ * @returns {number} v
+ * 
+ */
+
+
 function getVolume(l, w, h) {
     return l * w * h
 }
 
-console.log(getVolume(1,2,2));
+console.log(getVolume(1, 2, 2));
