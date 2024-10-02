8cf1f9b85a3565fbf6aee57f398a844e9a41a9c2
Belgacem-Dahmen <belgacem.dahmen@gmail.comn>
2024-10-02 12:23:12 +0200

    refactor : another variation of removing vowels

--- vowelRemover.js
+++ vowelRemover.js
@@ -12,8 +12,19 @@ function shortcut(string) {
     return string.split('').filter((e) => !Object.keys(vowels).includes(e)).join('')
 }
 
+
+
+
+function shortcut_regex(string){
+    return string.replace(/[aeiou]/g,'')
+  }
 console.log(shortcut('kacem'));
 console.log(shortcut('Hello'));
 console.log(shortcut('how are you today?'));
+
+
+console.log(shortcut_regex('kacem'));
+console.log(shortcut_regex('Hello'));
+console.log(shortcut_regex('how are you today?'));
 
 
