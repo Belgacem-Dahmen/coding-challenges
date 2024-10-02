6e81f34e6618bd6ab858ceb8fdf424b68376632d
Belgacem-Dahmen <belgacem.dahmen@gmail.comn>
2024-10-02 09:31:05 +0200

    Feature : a second opportunity to convert two rounded number

--- formattingDecimal.js
+++ formattingDecimal.js
@@ -10,4 +10,8 @@ function twoDecimalPlaces(n) {
     return Number(n.toFixed(2))
 }
 
+function twoDecimalPlaces_Math(n) {
+    return Math.round(n * 100) / 100
+}
 console.log(twoDecimalPlaces(1.6610));
+console.log(twoDecimalPlaces_Math(1.6610));
